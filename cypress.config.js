72544f45bdfe40485fb35443407e6d82cd507325
jainayush91 <37686862+jainayush91@users.noreply.github.com>
2023-01-14 14:48:11 +0000

    Changes made in config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+
       // implement node event listeners here
     },
   },
